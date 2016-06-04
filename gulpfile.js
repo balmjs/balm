764b410a2c2ed6627909c79d824c68980dfa001e
elf-mouse <ifmouz@gmail.com>
2016-06-04 08:40:09 +0800

    up

--- gulpfile.js
+++ gulpfile.js
@@ -67,5 +67,5 @@ gulp.task('clean', function () {
   return del('dist');
 });
 
-gulp.task('prepublish', ['nsp', 'babel']);
+gulp.task('prepublish', ['babel']);
 gulp.task('default', ['static', 'test']);
