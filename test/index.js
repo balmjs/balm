c1da7a1e74dc0a54ca384b317daa52ec35da543d
Elf-mousE <ifmouz@gmail.com>
2016-06-21 10:32:40 +0800

    update test

---
+++ test/index.js
@@ -0,0 +1,8 @@
+import assert from 'assert';
+import node from '../lib';
+
+describe('node', function () {
+  it('should have unit test!', function () {
+    assert(false, 'we expected this package author to add actual unit tests.');
+  });
+});
