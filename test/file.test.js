ab34e037b882eb5975e55313c2a18ea5d9760fea
Elf-mousE <ifmouz@gmail.com>
2017-06-01 11:34:13 +0800

    update test

--- test/file.test.js
+++ test/file.test.js
@@ -3,25 +3,27 @@ import assert from 'assert';
 import fs from 'fs';
 import balmConfig from './balm.config';
 
-describe('Test build files', function() {
-  let appDir = path.join(balmConfig.workspace, balmConfig.roots.source);
-  let cssDir = path.join(appDir, balmConfig.paths.source.css);
+if (process.platform !== 'win32') {
+  describe('Test build files', function() {
+    let appDir = path.join(balmConfig.workspace, balmConfig.roots.source);
+    let cssDir = path.join(appDir, balmConfig.paths.source.css);
 
-  describe('#sprites', function() {
-    let spritesDir = path.join(cssDir, 'sprites');
-    it('sprites: `path/to/workspace/app/styles/sprites`', function(done) {
-      fs.exists(spritesDir, function(exists) {
-        assert.ok(exists);
-        done();
+    describe('#sprites', function() {
+      let spritesDir = path.join(cssDir, 'sprites');
+      it('sprites: `path/to/workspace/app/styles/sprites`', function(done) {
+        fs.exists(spritesDir, function(exists) {
+          assert.ok(exists);
+          done();
+        });
       });
-    });
 
-    let svgDir = path.join(cssDir, 'svg');
-    it('svg: `path/to/workspace/app/styles/svg`', function(done) {
-      fs.exists(svgDir, function(exists) {
-        assert.ok(exists);
-        done();
+      let svgDir = path.join(cssDir, 'svg');
+      it('svg: `path/to/workspace/app/styles/svg`', function(done) {
+        fs.exists(svgDir, function(exists) {
+          assert.ok(exists);
+          done();
+        });
       });
     });
   });
-});
+}
