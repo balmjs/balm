37f3f2bdd03870ef44d5fff9cdcd12d20da0335c
Elf-mousE <ifmouz@gmail.com>
2017-06-02 11:21:23 +0800

    update test

--- test/file.test.js
+++ test/file.test.js
@@ -26,53 +26,53 @@ describe('Test sprites files', () => {
   });
 });
 
-describe('Test assets files', () => {
-  let publicDir = path.join(balmConfig.assets.root, balmConfig.assets.publicPath, balmConfig.assets.subDir);
-  let viewsDir = path.join(balmConfig.assets.root, 'views');
-
-  describe('#public', () => {
-    let cssFolder = path.join(publicDir, balmConfig.paths.target.css);
-    let jsFolder = path.join(publicDir, balmConfig.paths.target.js);
-    let imgFolder = path.join(publicDir, balmConfig.paths.target.img);
-    let fontFolder = path.join(publicDir, balmConfig.paths.target.font);
-
-    it('CSS: `path/to/remote/assets/public/web/css`', done => {
-      fs.exists(cssFolder, exists => {
-        assert.ok(exists);
-        done();
-      });
-    });
-
-    it('JS: `path/to/remote/assets/public/web/js`', done => {
-      fs.exists(jsFolder, exists => {
-        assert.ok(exists);
-        done();
-      });
-    });
-
-    it('Images: `path/to/remote/assets/public/web/img`', done => {
-      fs.exists(imgFolder, exists => {
-        assert.ok(exists);
-        done();
-      });
-    });
-
-    it('Fonts: `path/to/remote/assets/public/web/font`', done => {
-      fs.exists(fontFolder, exists => {
-        assert.ok(exists);
-        done();
-      });
-    });
-  });
-
-  describe('#views', () => {
-    let indexFile = path.join(viewsDir, 'home.blade.php');
-    console.log(indexFile);
-    it('Index file: `path/to/remote/assets/views/home.blade.php`', done => {
-      fs.exists(indexFile, exists => {
-        assert.ok(exists);
-        done();
-      });
-    });
-  });
-});
+// describe('Test assets files', () => {
+//   let publicDir = path.join(balmConfig.assets.root, balmConfig.assets.publicPath, balmConfig.assets.subDir);
+//   let viewsDir = path.join(balmConfig.assets.root, 'views');
+//
+//   describe('#public', () => {
+//     let cssFolder = path.join(publicDir, balmConfig.paths.target.css);
+//     let jsFolder = path.join(publicDir, balmConfig.paths.target.js);
+//     let imgFolder = path.join(publicDir, balmConfig.paths.target.img);
+//     let fontFolder = path.join(publicDir, balmConfig.paths.target.font);
+//
+//     it('CSS: `path/to/remote/assets/public/web/css`', done => {
+//       fs.exists(cssFolder, exists => {
+//         assert.ok(exists);
+//         done();
+//       });
+//     });
+//
+//     it('JS: `path/to/remote/assets/public/web/js`', done => {
+//       fs.exists(jsFolder, exists => {
+//         assert.ok(exists);
+//         done();
+//       });
+//     });
+//
+//     it('Images: `path/to/remote/assets/public/web/img`', done => {
+//       fs.exists(imgFolder, exists => {
+//         assert.ok(exists);
+//         done();
+//       });
+//     });
+//
+//     it('Fonts: `path/to/remote/assets/public/web/font`', done => {
+//       fs.exists(fontFolder, exists => {
+//         assert.ok(exists);
+//         done();
+//       });
+//     });
+//   });
+//
+//   describe('#views', () => {
+//     let indexFile = path.join(viewsDir, 'home.blade.php');
+//     console.log(indexFile);
+//     it('Index file: `path/to/remote/assets/views/home.blade.php`', done => {
+//       fs.exists(indexFile, exists => {
+//         assert.ok(exists);
+//         done();
+//       });
+//     });
+//   });
+// });
