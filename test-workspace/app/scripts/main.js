a1bbc01009847bf674c9dbaf641576726f584eee
Elf-mousE <ifmouz@gmail.com>
2017-07-19 06:39:28 +0800

    update public url test

--- test-workspace/app/scripts/main.js
+++ test-workspace/app/scripts/main.js
@@ -1,12 +1,12 @@
-// import hello from './hello';
-const hello = () => import('./hello');
+import hello from './hello';
+// const hello = () => import('./hello');
 
 // sync component
-// hello.greet();
+hello.greet();
 
 // async component
-hello().then(m => {
-  m.default.greet();
-});
+// hello().then(m => {
+//   m.default.greet();
+// });
 
 console.log('I\'m home page');
