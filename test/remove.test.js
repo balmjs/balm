781d0997921720e5dd6b4863edd6ebf052cc9229
elf-mouse <ifmouz@gmail.com>
2017-10-16 10:24:40 +0800

    update remove test

--- test/remove.test.js
+++ test/remove.test.js
@@ -1,12 +1,23 @@
 // describe('Remove Task', () => {
 //   beforeEach(() => balm.reset());
-//
+
 //   it('remove a file', done => {
 //     balm.afterTask = () => {
-//       fs.existsSync(`${workspace}/src/remove-me.txt`).should.be.false;
+//       fs.existsSync(`${workspace}/src/remove-file.txt`).should.be.false;
 //     };
-//     balm.go(mix => mix.remove('./src/remove-me.txt'));
-//
+//     balm.go(mix => mix.remove('./src/remove-file.txt'));
+
+//     runGulp(() => {
+//       done();
+//     });
+//   });
+
+//   it('remove a folder', done => {
+//     balm.afterTask = () => {
+//       fs.existsSync(`${workspace}/src/remove-folder`).should.be.false;
+//     };
+//     balm.go(mix => mix.remove('./src/remove-folder'));
+
 //     runGulp(() => {
 //       done();
 //     });
