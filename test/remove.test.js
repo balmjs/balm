5b0539616f9f9fca60e885423b51a252216688a8
Elf-mousE <ifmouz@gmail.com>
2017-10-22 18:10:02 +0800

    update remove test

--- test/remove.test.js
+++ test/remove.test.js
@@ -1,25 +1,39 @@
 // describe('Remove Task', () => {
 //   beforeEach(() => balm.reset());
-
+//
 //   it('remove a file', done => {
 //     balm.afterTask = () => {
-//       fs.existsSync(`${workspace}/src/remove-file.txt`).should.be.false;
+//       fs.existsSync(`${workspace}/src/remove/remove-file.txt`).should.be.false;
 //     };
-//     balm.go(mix => mix.remove('./src/remove-file.txt'));
-
+//     balm.go(mix => mix.remove('./src/remove/remove-file.txt'));
+//
 //     runGulp(() => {
 //       done();
 //     });
 //   });
-
-//   it('remove a folder', done => {
+//
+//   it('remove files', done => {
 //     balm.afterTask = () => {
-//       fs.existsSync(`${workspace}/src/remove-folder`).should.be.false;
+//       fs.existsSync(`${workspace}/src/remove/remove-folder/a.txt`).should.be.false;
+//       fs.existsSync(`${workspace}/src/remove/remove-folder/b.txt`).should.be.false;
 //     };
-//     balm.go(mix => mix.remove('./src/remove-folder'));
-
+//
+//     let input = ['./src/remove/remove-folder/a.txt', './src/remove/remove-folder/b.txt'];
+//     balm.go(mix => mix.remove(input));
+//
 //     runGulp(() => {
 //       done();
 //     });
 //   });
+//
+//   // it('remove a folder', done => {
+//   //   balm.afterTask = () => {
+//   //     fs.existsSync(`${workspace}/src/remove/remove-folder`).should.be.false;
+//   //   };
+//   //   balm.go(mix => mix.remove('./src/remove/remove-folder'));
+//   //
+//   //   runGulp(() => {
+//   //     done();
+//   //   });
+//   // });
 // });
