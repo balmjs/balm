3100563913d97a48154349482b302f92007ce663
Elf-mousE <ifmouz@gmail.com>
2018-04-19 18:14:23 +0800

    update config

--- test-workspace/gulpfile.babel.js
+++ test-workspace/gulpfile.babel.js
@@ -1,2 +1,11 @@
-require('./config/spa');
+var balm = require('../lib/main');
+
+balm.config = {
+  // debug: true
+  useDefault: true
+};
+
+// console.log(balm.config);
+
+// require('./config/spa');
 // require('./config/ssr/client');
