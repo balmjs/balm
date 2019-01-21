e13eed4fdc316c782e5e95d24d733067d1c21a63
Elf-mousE <ifmouz@gmail.com>
2019-01-21 17:53:04 +0800

    update test

--- test/tasks/default.js
+++ test/tasks/default.js
@@ -22,14 +22,23 @@ describe('Default Task', () => {
 
   it('verify balm task result', done => {
     const task = 'default';
-    const test = [
-      'dist/index.html',
-      'dist/manifest.json',
-      'dist/web/a/main.d2551023.css',
-      'dist/web/b/main.e31b0757.js',
-      'dist/web/c/logo.bae9298c.svg',
-      'dist/web/d/roboto-regular.f94d5e51.woff'
-    ];
+    const test = balm.config.production
+      ? [
+          'dist/index.html',
+          'dist/manifest.json',
+          'dist/web/a/main.d2551023.css',
+          'dist/web/b/main.e31b0757.js',
+          'dist/web/c/logo.bae9298c.svg',
+          'dist/web/d/roboto-regular.f94d5e51.woff'
+        ]
+      : [
+          '.tmp/index.html',
+          '.tmp/a/main.css',
+          '.tmp/a/main.css.map',
+          '.tmp/b/main.js',
+          '.tmp/c/img-icon-sprite.png',
+          '.tmp/fonts/roboto-regular.woff'
+        ];
 
     runTask({
       task,
