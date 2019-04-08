5a3fd196d129ead7f6d96b05b252a1f5ca76d739
Elf-mousE <ifmouz@gmail.com>
2019-04-08 15:52:32 +0800

    update test

--- test/tasks/private/extra.js
+++ test/tasks/private/extra.js
@@ -3,7 +3,7 @@ import ExtraTask from '../../../lib/tasks/private/extra';
 describe('Extra Task', () => {
   it('generate extra files in root directory', done => {
     const task = new ExtraTask();
-    const test = ['dist/favicon.ico', 'dist/manifest.json'];
+    const test = 'dist/favicon.ico';
 
     runTask({
       task,
