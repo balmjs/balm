2543f8a8e3a3784d756ed6a8115c6e4d87dddaef
Elf-mousE <ifmouz@gmail.com>
2019-06-11 15:12:35 +0800

    update test

--- test-revision/gulpfile.js
+++ test-revision/gulpfile.js
@@ -1,11 +1,24 @@
 const gulp = require('gulp');
 const revAll = require('gulp-rev-all');
+const del = require('del');
 
-gulp.task('default', () => {
+const cleanTask = () => {
+  return del('dist');
+};
+
+const revAllTask = () => {
   return gulp
     .src('app/**')
-    .pipe(revAll.revision())
+    .pipe(
+      revAll.revision({
+        fileNameManifest: 'rev-manifest.json',
+        dontRenameFile: ['.html', '.php'],
+        dontUpdateReference: ['.html', '.php']
+      })
+    )
     .pipe(gulp.dest('dist'))
     .pipe(revAll.manifestFile())
     .pipe(gulp.dest('dist'));
-});
+};
+
+gulp.task('default', gulp.series(cleanTask, revAllTask));
