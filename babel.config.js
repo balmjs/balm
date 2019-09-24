f321bc628814092fbdf8c22d6145d186381f67cf
Elf-mousE <ifmouz@gmail.com>
2019-09-24 11:01:28 +0800

    update babel

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,10 @@
 module.exports = {
   presets: ['@babel/preset-env', '@babel/preset-typescript'],
+  plugins: [
+    '@babel/plugin-transform-runtime',
+    '@babel/proposal-class-properties',
+    '@babel/plugin-proposal-object-rest-spread'
+  ],
   env: {
     test: {
       plugins: ['istanbul']
