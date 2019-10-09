4ddfa3aa9179583721ac84457179ec86bb3f7e88
Elf-mousE <ifmouz@gmail.com>
2019-10-09 17:09:39 +0800

    update

--- src/config/pwa.ts
+++ src/config/pwa.ts
@@ -1,6 +1,12 @@
 const enabled = false;
 const workboxSw = 'node_modules/workbox-sw/build/workbox-sw.js';
 const mode = 'generateSW'; // 'generateSW' or 'injectManifest'
+/**
+ * Workbox Build options
+ *
+ * @reference https://developers.google.com/web/tools/workbox/modules/workbox-build#full_generatesw_config
+ * @reference https://developers.google.com/web/tools/workbox/modules/workbox-build#full_injectmanifest_config
+ */
 const options: object = {};
 const swSrcFilename = 'service-worker.js';
 const swDestFilename = 'sw.js';
