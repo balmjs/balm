222da1de62cc4f493bf19d9020bd4d90ebbfbdfc
Elf-mousE <ifmouz@gmail.com>
2019-11-02 18:14:53 +0800

    update test

--- test/app/api.spec.ts
+++ test/app/api.spec.ts
@@ -1,6 +1,6 @@
 import { cleanup, runTest } from './test';
 
-const targetDir = '.manual';
+const targetDir = '.output';
 
 describe('balm hooks', function() {
   beforeEach(function() {
@@ -11,20 +11,20 @@ describe('balm hooks', function() {
 
   describe('stylesheets', function() {
     afterEach(function() {
-      // cleanup(targetDir);
+      cleanup(targetDir);
     });
 
     ['sass', 'less', 'css'].forEach(extname => {
       const api = extname;
+      const input = `src/styles/main.${extname === 'sass' ? 'scss' : extname}`;
+      const output = `${targetDir}/${extname}`;
+
       it(api, function(done) {
         runTest(
           {
-            testCase: `${targetDir}/${extname}/main.css`,
+            testCase: `${output}/main.css`,
             hook: function(mix: any) {
-              mix[api](
-                `src/styles/main.${extname === 'sass' ? 'scss' : extname}`,
-                `${targetDir}/${extname}`
-              );
+              mix[api](input, output);
             }
           },
           done
@@ -50,7 +50,72 @@ describe('balm hooks', function() {
   //   });
   // });
 
-  // describe('files & directories', function() {});
+  describe('files & directories', function() {
+    it('copies a file to a new location', function(done) {
+      const input = 'src/copy/foo/foo.txt';
+      const output = `${targetDir}/copy-dest`;
+
+      runTest(
+        {
+          testCase: `${output}/foo.txt`,
+          hook: function(mix: any) {
+            mix.copy(input, output);
+          }
+        },
+        done
+      );
+    });
+
+    it('copies and renames a file to a new location', function(done) {
+      const input = 'src/copy/foo/foo.txt';
+      const output = `${targetDir}/copy-dest`;
+      const renameOptions = {
+        basename: 'changed'
+      };
+
+      runTest(
+        {
+          testCase: `${output}/changed.txt`,
+          hook: function(mix: any) {
+            mix.copy(input, output, {
+              renameOptions
+            });
+          }
+        },
+        done
+      );
+    });
+
+    it('copies an array of folder paths to a new location', function(done) {
+      const input = ['./src/copy/foo/*', './src/copy/bar/*'];
+      const output = `${targetDir}/copy-dest/foobar`;
+
+      runTest(
+        {
+          testCase: [`${output}/foo.txt`, `${output}/bar.txt`],
+          hook: function(mix: any) {
+            mix.copy(input, output);
+          }
+        },
+        done
+      );
+    });
+
+    it('copies a folder with a period in its name to a new location', function(done) {
+      const input = './src/copy/foo.bar/*';
+      const output = `${targetDir}/copy-dest/some.dir`;
+
+      runTest(
+        {
+          testCase: `${output}/baz.txt`,
+          hook: function(mix: any) {
+            mix.copy(input, output);
+          }
+        },
+        done
+      );
+    });
+  });
 
   // describe('cache', function() {});
 
