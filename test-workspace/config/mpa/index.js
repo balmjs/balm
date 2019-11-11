d83a29e5d16434c16657650a26918b14bd69f144
elf-mouse <ifmouz@gmail.com>
2019-11-11 21:20:26 +0800

    update

--- test-workspace/config/mpa/index.js
+++ test-workspace/config/mpa/index.js
@@ -1,32 +1,26 @@
 const balm = require('../balm');
-let balmConfig = require('../balmrc');
+const balmConfig = require('../balmrc');
 
-const scripts = {
-  entry: {
-    // lib: ['jquery', 'lodash', 'moment'],
-    lib1: ['jquery'],
-    lib2: ['lodash'],
-    lib3: ['moment'],
-    'page-a': './mpa/scripts/page-1.js',
-    'page-b': './mpa/scripts/page-2.js',
-    'page-c': './mpa/scripts/page-3.js'
-  },
-  // extractAllVendors: true,
-  options: {
-    compress: {
-      drop_console: false
-    }
-  }
-};
-
-balmConfig = Object.assign(balmConfig, {
+balm.config = Object.assign(balmConfig, {
   roots: {
     source: 'mpa'
   },
-  scripts,
-  cache: true
+  scripts: {
+    entry: {
+      // lib: ['jquery', 'lodash', 'moment'],
+      lib1: ['jquery'],
+      lib2: ['lodash'],
+      lib3: ['moment'],
+      'page-a': './mpa/scripts/page-1.js',
+      'page-b': './mpa/scripts/page-2.js',
+      'page-c': './mpa/scripts/page-3.js'
+    },
+    options: {
+      compress: {
+        drop_console: false
+      }
+    }
+  }
 });
-
-balm.config = balmConfig;
 
 balm.go();
