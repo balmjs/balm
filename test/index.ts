e8926237b2823a48fa823714a445c3aa555fb69d
Elf-mousE <ifmouz@gmail.com>
2020-11-17 16:22:45 +0800

    test: update

--- test/index.ts
+++ test/index.ts
@@ -1,11 +1,11 @@
+import './app';
+import './app/hooks';
 import './utilities';
 import './tasks';
 import './middlewares';
 import './bundler';
 import './bootstrap/config_check.spec';
 import './bootstrap/index.spec';
-import './app';
-import './app/hooks';
 
 const balmConfigDefaults = Object.assign(
   {},
