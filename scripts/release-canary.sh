708117e4d2df765e3c2eeb50cd381a6710e04ab4
Elf-mousE <ifmouz@gmail.com>
2020-11-19 18:08:59 +0800

    chore: update config

--- scripts/release-canary.sh
+++ scripts/release-canary.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 npm run build
-lerna publish --canary --preid canary --no-git-tag-version --no-push --dist-tag next --force-publish balm-core
+lerna publish --canary --preid canary --no-git-tag-version --no-push --dist-tag canary --force-publish balm-core
