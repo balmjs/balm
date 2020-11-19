176fb022ed039726807334fa25b5024594a451de
Elf-mousE <ifmouz@gmail.com>
2020-11-19 18:20:02 +0800

    chore: update config

--- scripts/release-canary.sh
+++ scripts/release-canary.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 npm run build
-lerna publish --no-git-tag-version --no-push --canary preminor --preid canary.$(git rev-parse --short HEAD) --yes --exact from-package --dist-tag canary --force-publish balm-core
+lerna publish --no-git-tag-version --no-push --canary preminor --preid canary.$(git rev-parse --short HEAD) --yes --exact --dist-tag canary --force-publish balm-core
