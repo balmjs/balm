79579084741e07653ff0cfd45183cb1fb8d78cd9
Elf-mousE <ifmouz@gmail.com>
2020-12-06 09:16:28 +0800

    test: update config

--- test-workspace/balm.env.js
+++ test-workspace/balm.env.js
@@ -1,3 +1,6 @@
 const BALM_ROOT = '/path/to/balm';
 
 process.env.BALM_CORE = `${BALM_ROOT}/packages/balm-core`;
+
+// process.env.USE_LOCAL_WEBPACK = true;
+// process.env.WEBPACK = require.resolve('webpack');
