64ebab20dc9867c867decc9223cad227650c4ed7
elf-mouse <ifmouz@gmail.com>
2020-12-31 12:28:32 +0800

    feat: add cache require paths

--- packages/balm-core/src/hello.ts
+++ packages/balm-core/src/hello.ts
@@ -1,16 +1,6 @@
-import Module from 'module';
-import loading from './utilities/loading';
+import cacheBalmCore from './utilities/cache-core';
 
 global.BalmJS = {} as any;
 BalmJS.loading = true;
 
-const _require = Module.prototype.require;
-
-(Module as any).prototype.require = function cachePathsRequire(name: string) {
-  const pathToLoad = (Module as any)._resolveFilename(name, this);
-  if (BalmJS.loading) {
-    loading.render();
-  }
-
-  return _require.call(this, pathToLoad);
-};
+cacheBalmCore();
