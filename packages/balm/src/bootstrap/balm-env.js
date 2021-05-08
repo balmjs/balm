8fabbf912a875a565e9f17616a9194e0a47d8267
elf-mouse <ifmouz@gmail.com>
2021-05-08 14:12:00 +0800

    chore: update

--- packages/balm/src/bootstrap/balm-env.js
+++ packages/balm/src/bootstrap/balm-env.js
@@ -1,21 +1,19 @@
 import path from 'node:path';
 import fs from 'node:fs';
-// import { createRequire } from 'node:module';
 
 const balmEnvFile = path.join(process.cwd(), 'balm.env.js');
+const webpackLocalModule = path.resolve(
+  fs.realpathSync(process.cwd()),
+  'node_modules',
+  'webpack/lib/index.js'
+);
 
 const getBalmEnv = async () => {
   if (fs.existsSync(balmEnvFile)) {
     await import(balmEnvFile);
 
-    if (process.env.USE_LOCAL_WEBPACK) {
-      // const require = createRequire(import.meta.url);
-      // process.env.WEBPACK = require.resolve('webpack');
-      process.env.WEBPACK = path.resolve(
-        fs.realpathSync(process.cwd()),
-        'node_modules',
-        'webpack/lib/index.js'
-      );
+    if (fs.existsSync(webpackLocalModule)) {
+      process.env.WEBPACK = webpackLocalModule;
     }
   }
 };
