3a0c587b96f4156ceda3f9f53113deb6e75cf72e
Elf-mousE <ifmouz@gmail.com>
2022-04-08 12:37:12 +0800

    test: update

--- test-workspace/config/spa/extract-css.js
+++ test-workspace/config/spa/extract-css.js
@@ -1,5 +1,3 @@
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-
 module.exports = {
   entry: {
     'css-a': './spa/scripts/css-page-a.js',
@@ -9,38 +7,5 @@ module.exports = {
   htmlPluginOptions: {
     title: ['Page 1', 'Page 2']
   },
-  defaultLoaders: {
-    css: false
-  },
-  loaders: [
-    {
-      test: /\.css$/i,
-      use: [MiniCssExtractPlugin.loader, 'css-loader']
-    }
-  ],
-  plugins: [
-    new MiniCssExtractPlugin({
-      filename: 'asset/[name].[contenthash:8].css',
-      chunkFilename: 'asset/[name].[contenthash:8].css'
-    })
-  ],
-  optimization: {
-    splitChunks: {
-      cacheGroups: {
-        aStyles: {
-          type: 'css/mini-extract',
-          name: 'css-a',
-          chunks: (chunk) => chunk.name === 'css-a',
-          enforce: true
-        },
-        bStyles: {
-          type: 'css/mini-extract',
-          name: 'css-b',
-          chunks: (chunk) => chunk.name === 'css-b',
-          enforce: true
-        }
-      }
-    }
-  }
-  // extractCss: true
+  extractCss: true
 };
