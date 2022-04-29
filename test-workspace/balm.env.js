b79a3ed8a33e15ee334acaf7e3d57061ac8907cf
Elf-mousE <ifmouz@gmail.com>
2022-04-29 10:04:29 +0800

    test: update

--- test-workspace/balm.env.js
+++ test-workspace/balm.env.js
@@ -1,4 +1,4 @@
 const BALM_ROOT = '/Users/elf-mouse/www/balmjs/balm';
 
+process.env.BALM_ROOT = BALM_ROOT;
 process.env.BALM_CORE = `${BALM_ROOT}/packages/balm-core`;
-process.env.BALM = BALM_ROOT;
