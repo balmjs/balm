755fef69ee2c2a31d403335db5f42f8f0f5a0f87
elf-mouse <ifmouz@gmail.com>
2023-09-26 16:06:15 +0800

    chore: clean code

--- packages/balm-core/src/config/scripts.ts
+++ packages/balm-core/src/config/scripts.ts
@@ -22,14 +22,6 @@ const minify = false;
  */
 const minifyOptions: MinifyOptions = {
   ecma: 5,
-  parse: {
-    // We want terser to parse ecma 8 code. However, we don't want it
-    // to apply any minification steps that turns valid ecma 5 code
-    // into invalid ecma 5 code. This is why the 'compress' and 'output'
-    // sections only apply transformations that are ecma 5 safe
-    // https://github.com/facebook/create-react-app/pull/4234
-    ecma: 2017
-  },
   compress: {
     // Disabled because of an issue with Uglify breaking seemingly valid code:
     // https://github.com/facebook/create-react-app/issues/2376
@@ -45,7 +37,7 @@ const minifyOptions: MinifyOptions = {
   mangle: {
     safari10: true
   },
-  output: {
+  format: {
     comments: false,
     // Turned on because emoji and regex is not minified properly using default
     // https://github.com/facebook/create-react-app/issues/2488
