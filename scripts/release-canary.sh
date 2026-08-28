52fec9d5dc85a0235c76196e7daa15e1a96214e4
elf-mouse <elf-mouse@users.noreply.github.com>
2026-08-28 02:01:43 -0700

    chore: update config

--- scripts/release-canary.sh
+++ scripts/release-canary.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
+if [ -f ~/.zshrc ]; then
+  ZSH_NPM_TOKEN=$(grep -E '^export NPM_TOKEN=' ~/.zshrc | cut -d '=' -f2-)
+  if [ -n "$ZSH_NPM_TOKEN" ]; then
+    export NPM_TOKEN="$ZSH_NPM_TOKEN"
+  fi
+fi
+
 npm run build
 lerna publish premajor --no-git-tag-version --no-push --preid canary.$(git rev-parse --short HEAD) --exact --dist-tag canary --force-publish balm-core
